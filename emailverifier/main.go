55d50ba4188d26ba2bedac48fd5fc69b3b580555
Kevin Wafula <119099815+djangocoder101@users.noreply.github.com>
2023-06-21 20:02:20 +0300

    Add files via upload

---
+++ emailverifier/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Domain hasMx, hasSpf, spfRecord, hasDMARC, dmarcRecord\n")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+
+}
+
+func checkDomain(domain string) {
+	var hasMx, hasSpf, hasDMARC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMx = true
+	}
+
+	spfRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range spfRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSpf = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMx, hasSpf, spfRecord, hasDMARC, dmarcRecord)
+}
